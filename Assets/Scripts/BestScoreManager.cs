5d256f40ffec036cae42e96d9aaa04c1e9f3c694
Drengon <95448077+Drengon@users.noreply.github.com>
2021-12-04 16:36:42 -0500

    Fixed my mistake
    Yup!

---
+++ Assets/Scripts/BestScoreManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BestScoreManager : MonoBehaviour
+{
+    public Input PlayerName;
+    public Input BestScore;
+
+
+}
